f1f3ae111db2af4cea809cb885fca8efeabf20f7
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-12-06 21:18:17 -0800

    Rebase from 'debian/crystal/rmw_opensplice_cpp'

--- src/qos.hpp
+++ src/qos.hpp
@@ -15,7 +15,21 @@
 #ifndef QOS_HPP_
 #define QOS_HPP_
 
+#ifdef __clang__
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wmismatched-tags"
+#endif
+#if defined(_MSC_VER)
+# pragma warning(push)
+# pragma warning(disable: 4099)
+#endif
 #include <ccpp_dds_dcps.h>
+#if defined(_MSC_VER)
+# pragma warning(pop)
+#endif
+#ifdef __clang__
+# pragma GCC diagnostic pop
+#endif
 #include <dds_dcps.h>
 #include <u_instanceHandle.h>
 
