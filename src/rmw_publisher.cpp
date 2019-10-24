92ae1417f5c60414731650ba882d66d5982c7ef8
Michael Carroll <michael@openrobotics.org>
2019-10-23 23:27:26 -0500

    Rebase from 'release/eloquent/rmw_opensplice_cpp'

--- src/rmw_publisher.cpp
+++ src/rmw_publisher.cpp
@@ -85,7 +85,8 @@ rmw_create_publisher(
   const rmw_node_t * node,
   const rosidl_message_type_support_t * type_supports,
   const char * topic_name,
-  const rmw_qos_profile_t * qos_profile)
+  const rmw_qos_profile_t * qos_profile,
+  const rmw_publisher_options_t * publisher_options)
 {
   if (!node) {
     RMW_SET_ERROR_MSG("node handle is null");
@@ -106,6 +107,11 @@ rmw_create_publisher(
 
   if (!qos_profile) {
     RMW_SET_ERROR_MSG("qos_profile is null");
+    return nullptr;
+  }
+
+  if (!publisher_options) {
+    RMW_SET_ERROR_MSG("publisher_options is null");
     return nullptr;
   }
 
@@ -244,7 +250,9 @@ rmw_create_publisher(
     goto fail;
   }
   memcpy(const_cast<char *>(publisher->topic_name), topic_name, strlen(topic_name) + 1);
+  publisher->options = *publisher_options;
 
+  publisher->can_loan_messages = false;
   return publisher;
 fail:
   if (publisher) {
@@ -369,6 +377,33 @@ rmw_publisher_assert_liveliness(const rmw_publisher_t * publisher)
     RMW_SET_ERROR_MSG("failed to assert liveliness of datawriter");
   }
   return result;
+}
+
+rmw_ret_t
+rmw_borrow_loaned_message(
+  const rmw_publisher_t * publisher,
+  const rosidl_message_type_support_t * type_support,
+  void ** ros_message)
+{
+  (void) publisher;
+  (void) type_support;
+  (void) ros_message;
+
+  RMW_SET_ERROR_MSG("rmw_borrow_loaned_message not implemented for rmw_opensplice_cpp");
+  return RMW_RET_UNSUPPORTED;
+}
+
+rmw_ret_t
+rmw_return_loaned_message_from_publisher(
+  const rmw_publisher_t * publisher,
+  void * loaned_message)
+{
+  (void) publisher;
+  (void) loaned_message;
+
+  RMW_SET_ERROR_MSG(
+    "rmw_return_loaned_message_from_publisher not implemented for rmw_opensplice_cpp");
+  return RMW_RET_UNSUPPORTED;
 }
 
 rmw_ret_t
